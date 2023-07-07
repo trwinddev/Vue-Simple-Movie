9866cb4abf7f735a41dee036531ae638c3bf0342
Ngô Trọng Phong <ngotrongphong.dev@gmail.com>
2023-07-07 16:25:37 +0700

    fix text

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,15 +1,15 @@
 <template>
   <banner></banner>
   <section class="movies-layout page-container pb-20">
-    <h2 class="capitalize text-white mb-5 text-3xl font-bold">Now playing</h2>
+    <h2 class="capitalize mb-5 text-3xl font-bold light:text-white">Now playing</h2>
     <movie-list type="now_playing"></movie-list>
   </section>
   <section class="movies-layout page-container pb-20">
-    <h2 class="capitalize text-white mb-5 text-3xl font-bold">Popular</h2>
+    <h2 class="capitalize mb-5 text-3xl font-bold light:text-white">Popular</h2>
     <movie-list type="popular"></movie-list>
   </section>
   <section class="movies-layout page-container pb-20">
-    <h2 class="capitalize text-white mb-5 text-3xl font-bold">Top rated</h2>
+    <h2 class="capitalize mb-5 text-3xl font-bold light:text-white">Top rated</h2>
     <movie-list type="top_rated"></movie-list>
   </section>
 </template>
